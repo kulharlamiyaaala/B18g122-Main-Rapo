e21f4b99017e3b0666c1f607df5eb96a216ec0bd
kulharlamiyaaala <kulhar2100@gmail.com>
2026-08-13 08:06:08 +0530

    remove backend

--- environments/prod/provider.tf
+++ environments/prod/provider.tf
@@ -6,13 +6,6 @@ terraform {
       version = "4.80.0"
     }
   }
-
-  backend "azurerm" {
-    resource_group_name  = "rg-terraform-state"
-    storage_account_name = "sttfstateb18g122"
-    container_name       = "tfstate"
-    key                  = "prod.terraform.tfstate"
-  }
 }
 
 provider "azurerm" {
