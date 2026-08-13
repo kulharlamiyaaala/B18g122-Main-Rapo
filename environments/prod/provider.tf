827ff7b4e18bae198248e409d8ea715777aec094
kulharlamiyaaala <kulhar2100@gmail.com>
2026-08-13 07:52:14 +0530

    backend

--- environments/prod/provider.tf
+++ environments/prod/provider.tf
@@ -6,6 +6,13 @@ terraform {
       version = "4.80.0"
     }
   }
+
+  backend "azurerm" {
+    resource_group_name  = "rg-terraform-state"
+    storage_account_name = "sttfstateb18g122"
+    container_name       = "tfstate"
+    key                  = "prod.terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
